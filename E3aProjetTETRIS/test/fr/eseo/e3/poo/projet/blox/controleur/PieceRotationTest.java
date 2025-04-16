f27ef3e0ea7a5126d7b36afdbdf8183414807b18
Thomas <91543762+torkadess@users.noreply.github.com>
2025-04-16 12:17:06 +0200

    Delete E3aProjetTETRIS directory

--- E3aProjetTETRIS/test/fr/eseo/e3/poo/projet/blox/controleur/PieceRotationTest.java
+++
@@ -1,5 +0,0 @@
-package fr.eseo.e3.poo.projet.blox.controleur;
-
-public class PieceRotationTest {
-
-}
--- E3aProjetTETRIS/test/fr/eseo/e3/poo/projet/blox/vue/VueTasTest.java
+++
@@ -1,7 +0,0 @@
-package fr.eseo.e3.poo.projet.blox.vue;
-
-public class VueTasTest {
-	public static void main(String[] args) {
-		
-	}
-}
